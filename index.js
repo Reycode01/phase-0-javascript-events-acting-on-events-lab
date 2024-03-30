45d99f34b3c615acc4908945ee89649534fa167f
humphrey <writernic89@gmail.com>
2024-03-31 00:51:14 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  const left = dodger.style.left.replace('px', '');
+  const leftPosition = parseInt(left, 10);
+
+  if (leftPosition > 0) {
+    dodger.style.left = `${leftPosition - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const left = dodger.style.left.replace('px', '');
+  const leftPosition = parseInt(left, 10);
+  const rightEdge = 360; // Right edge of the game field
+
+  if (leftPosition < rightEdge) {
+    dodger.style.left = `${leftPosition + 1}px`;
+  }
+}
